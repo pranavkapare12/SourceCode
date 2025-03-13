b0d5642a126f33383a12012c2344f6ae36212c02
Pranav Kapare <151979852+pranavkapare12@users.noreply.github.com>
2025-03-13 18:37:27 +0530

    Update email.php

--- source code/Forgot_password/email.php
+++ source code/Forgot_password/email.php
@@ -12,9 +12,6 @@ function smtp_mailer($to,$subject, $msg){
 	$mail->IsHTML(true);
 	$mail->CharSet = 'UTF-8';
 	//$mail->SMTPDebug = 2; 
-	$mail->Username = "sourcecode411028@gmail.com";
-	$mail->Password = "------------------------";
-	$mail->SetFrom("sourcecode411028@gmail.com");
 	$mail->Subject = $subject;
 	$mail->Body =$msg;
 	$mail->AddAddress($to);
